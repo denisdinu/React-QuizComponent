6405eac795d018f56be0c554df66a11fd3c6d779
denisdinu <45061663+denisdinu@users.noreply.github.com>
2019-05-19 17:09:47 +0300

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,7 +12,9 @@ class Quiz extends Component {
       }
 
     showNextQuestion(){
-        this.setState({quiz_position: 1 });
+        this.setState((state) => {
+            return { quiz_position:  1 }
+          })
     }
 
   render() {
