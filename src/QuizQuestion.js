479155f3da4c0fdef6b9d14884eddced8183dcbc
denisdinu <45061663+denisdinu@users.noreply.github.com>
2019-05-19 17:08:10 +0300

    Completed the module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,7 +5,7 @@ import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component{
     handleClick (buttonText){
-        if(button_text === this.props.quiz_question.answer){
+        if(buttonText === this.props.quiz_question.answer){
             this.props.showNextQuestionHandler();
         }
     }
